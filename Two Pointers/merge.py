935676e520ff82f904a1469783bfd7cb5201f69e
yash mantri <mantriyash2025@gmail.com>
2022-06-03 22:22:48 +0530

    Problem Solving Day 5

---
+++ Two Pointers/merge.py
@@ -0,0 +1,19 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        idx = m + n -1
+        i = m - 1
+        j = n - 1
+        while j >= 0 and i >= 0:
+            print(nums2, nums1)
+            if nums1[i] > nums2[j]:
+                nums1[idx] = nums1[i]
+                i -= 1
+            else:
+                nums1[idx] = nums2[j]
+                j -= 1
+            idx -= 1
+        if j >= 0: # If nums2 have more element than nums1 
+            nums1[:idx+1] = nums2[:j+1]
